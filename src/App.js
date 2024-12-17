89fb8f862611e615f2065f85134617143eb82c34
ychung-mot <young-jin.chung@dxcas.com>
2024-12-17 10:48:21 -0800

    chore: update token button

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React from "react";
 import { useKeycloak } from "@react-keycloak/web";
+import Display from "./Display";
 
 const App = () => {
   const { keycloak, initialized } = useKeycloak();
 
-  const epochToReadableTime = (epochTime) => {
-    const date = new Date(epochTime * 1000);
-
-    const options = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    };
-
-    return date.toLocaleDateString("en-US", options);
-  };
-
   if (!initialized) return <div>Loading...</div>;
 
   return keycloak.authenticated ? (
-    <div>
-      <h1>Welcome, {keycloak.tokenParsed?.preferred_username}</h1>
-      <h2>Access Token: {keycloak.token.slice(-5)}</h2>
-      <h2>Refresh Token: {keycloak.refreshToken.slice(-5)}</h2>
-      <h2>Expiration: {epochToReadableTime(keycloak.tokenParsed.exp)}</h2>
-    </div>
+    <Display keycloak={keycloak} />
   ) : (
     <div>Redirecting to login...</div>
   );
---
+++ src/Display.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+
+const Display = ({ keycloak }) => {
+  const [kc, setKc] = useState(keycloak);
+
+  const epochToReadableTime = (epochTime) => {
+    if (!epochTime) return "";
+
+    const date = new Date(epochTime * 1000);
+
+    const options = {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    };
+
+    return date.toLocaleDateString("en-US", options);
+  };
+
+  const handleClick = async () => {
+    const refreshed = await keycloak.updateToken(30);
+    if (refreshed) setKc(keycloak);
+  };
+
+  return (
+    <div>
+      <h1>Welcome, {kc?.tokenParsed?.preferred_username}</h1>
+      <h2>Access Token: {kc?.token?.slice(-5)}</h2>
+      <h2>Refresh Token: {kc?.refreshToken?.slice(-5)}</h2>
+      <h2>Expiration: {epochToReadableTime(kc?.tokenParsed?.exp)}</h2>
+      <button onClick={handleClick}>Refresh Token</button>
+    </div>
+  );
+};
+
+export default Display;
