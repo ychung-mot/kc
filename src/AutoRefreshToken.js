517aa558e6ff6541ab1b0e4feb73905f015f6a98
ychung-mot <young-jin.chung@dxcas.com>
2024-12-18 09:43:41 -0800

    auto refresh token

--- src/AutoRefreshToken.js
+++ src/AutoRefreshToken.js
@@ -3,51 +3,49 @@ import { useKeycloak } from "@react-keycloak/web";
 
 const AutoRefreshToken = ({ children, setKc }) => {
   const LOCK_CHANNEL = "kc-token-refresh";
-  const LOCK_TIMEOUT = 1 * 60 * 1000 - 10000; // token expiration in minutes * 60 seconds - 20 seconds
+  const LOCK_TIMEOUT = 1 * 60 * 1000 - 10000; // Token expiration in minutes * 60 seconds - 10 seconds
   const { keycloak } = useKeycloak();
 
   useEffect(() => {
     if (!keycloak) return;
 
-    // Create a broadcast channel for token synchronization
     const refreshChannel = new BroadcastChannel(LOCK_CHANNEL);
     let lastUpdate = Date.now();
 
-    const getDate = () =>
-      new Date().toLocaleTimeString("en-US", {
+    const getDate = (date = new Date()) =>
+      date.toLocaleTimeString("en-US", {
         hour: "2-digit",
         minute: "2-digit",
         second: "2-digit",
       });
 
-    const delay = async () => {
-      await new Promise((resolve) => setTimeout(resolve, 1000));
-    };
+    const delay = (ms = 1000) =>
+      new Promise((resolve) => setTimeout(resolve, ms));
 
     const refreshToken = async () => {
-      if (lastUpdate && Date.now() - parseInt(lastUpdate) < LOCK_TIMEOUT) {
+      if (Date.now() - lastUpdate < LOCK_TIMEOUT) {
         return;
       }
 
       const newLastUpdate = Date.now();
+      lastUpdate = newLastUpdate;
 
       refreshChannel.postMessage({
         type: "LAST_UPDATE",
         lastUpdate: newLastUpdate,
       });
 
-      await delay();
+      await delay(); // Introduce a small delay to allow other tabs to process the message
 
       if (lastUpdate !== newLastUpdate) {
-        console.log(`Another tab is trying to lock`);
+        console.log("Another tab has taken the lock.");
         return;
       }
 
-      console.log(`Locked: ${getDate()}`);
+      console.log(`Locked for token refresh at ${getDate()}`);
 
       try {
         const refreshed = await keycloak.updateToken(30);
-
         if (refreshed) {
           const tokenUpdate = {
             type: "TOKEN_UPDATE",
@@ -56,11 +54,11 @@ const AutoRefreshToken = ({ children, setKc }) => {
             idToken: keycloak.idToken,
           };
 
-          // Broadcast token update to all tabs
-          refreshChannel.postMessage(tokenUpdate);
+          setKc({ ...keycloak });
 
+          refreshChannel.postMessage(tokenUpdate);
           console.log(
-            `Token refreshed: ${keycloak.token.slice(-5)} ${getDate()}`
+            `Token refreshed: ${keycloak.token.slice(-5)} at ${getDate()}`
           );
         }
       } catch (error) {
@@ -68,47 +66,46 @@ const AutoRefreshToken = ({ children, setKc }) => {
       }
     };
 
-    // Token refresh interval
     const interval = setInterval(() => {
       if (keycloak.token && keycloak.isTokenExpired(30)) {
         refreshToken();
       }
-    }, 10000);
+    }, 10000); // Check every 30 seconds, adjust as needed
 
-    // Handle messages from other tabs
     const handleBroadcastMessage = (event) => {
       switch (event.data.type) {
         case "LAST_UPDATE":
           lastUpdate = event.data.lastUpdate;
-          console.log(`LastUpdate updated across tabs: ${lastUpdate}`);
+          console.log(
+            `LastUpdate synchronized across tabs: ${getDate(
+              new Date(lastUpdate)
+            )}`
+          );
           break;
 
         case "TOKEN_UPDATE":
           if (event.data.token) {
-            // Update Keycloak's in-memory tokens
             keycloak.token = event.data.token;
             keycloak.refreshToken = event.data.refreshToken;
             keycloak.idToken = event.data.idToken;
 
             setKc({ ...keycloak });
-
             console.log(
               `Token updated across tabs: ${keycloak.token.slice(
                 -5
-              )} ${getDate()}`
+              )} at ${getDate()}`
             );
           }
           break;
 
         default:
+          break;
       }
     };
 
-    // Add broadcast channel message listener
     refreshChannel.addEventListener("message", handleBroadcastMessage);
 
     return () => {
-      // Cleanup
       clearInterval(interval);
       refreshChannel.removeEventListener("message", handleBroadcastMessage);
       refreshChannel.close();
