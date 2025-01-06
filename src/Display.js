cff8a6d2393c34506ce2f34ac796ec5d9091ceb1
ychung-mot <young-jin.chung@dxcas.com>
2025-01-06 12:36:46 -0800

    chore: display

--- src/Display.js
+++ src/Display.js
@@ -4,8 +4,6 @@ const Display = ({ kc, setKc }) => {
   return (
     <div>
       <h1>Welcome, {kc?.tokenParsed?.preferred_username}</h1>
-      <h2>Access Token: {kc?.token?.slice(-5)}</h2>
-      <h2>Refresh Token: {kc?.refreshToken?.slice(-5)}</h2>
     </div>
   );
 };
