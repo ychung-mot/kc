84473fc3ad877ede7f08b25271cd9450d2b00d05
ychung-mot <young-jin.chung@dxcas.com>
2025-01-06 12:28:14 -0800

    display

--- src/Display.js
+++ src/Display.js
@@ -1,31 +1,11 @@
 import React from "react";
 
 const Display = ({ kc, setKc }) => {
-  const epochToReadableTime = (epochTime) => {
-    if (!epochTime) return "";
-
-    const date = new Date(epochTime * 1000);
-
-    const options = {
-      weekday: "long",
-      year: "numeric",
-      month: "long",
-      day: "numeric",
-      hour: "2-digit",
-      minute: "2-digit",
-      second: "2-digit",
-    };
-
-    return date.toLocaleDateString("en-US", options);
-  };
-
   return (
     <div>
       <h1>Welcome, {kc?.tokenParsed?.preferred_username}</h1>
       <h2>Access Token: {kc?.token?.slice(-5)}</h2>
       <h2>Refresh Token: {kc?.refreshToken?.slice(-5)}</h2>
-      <h2>Expiration: {epochToReadableTime(kc?.tokenParsed?.exp)}</h2>
-      {/* <button onClick={handleClick}>Refresh Token</button> */}
     </div>
   );
 };
